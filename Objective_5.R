f07a92fe45489d2bf0001a65c28be7e1c9884699
Manuel Vargas <mvargas5897@student.palomar.edu>
2023-04-08 07:49:48 -0700

    Loading CSV files

--- Objective_5.R
+++ Objective_5.R
@@ -1,2 +1,4 @@
-
+# Load the CSV files for COVID-19 data
+confirmed_cases <- read.csv("https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv", stringsAsFactors = FALSE)
+deaths <- read.csv("https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_deaths_global.csv", stringsAsFactors = FALSE)
 

6d945ce50e42fc5a5f1b65a15a9341cccfa623f9
Manuel Vargas <mvargas5897@student.palomar.edu>
2023-04-08 07:51:25 -0700

    Extracting Unique Country Names

--- Objective_5.R
+++ Objective_5.R
@@ -2,3 +2,5 @@
 confirmed_cases <- read.csv("https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv", stringsAsFactors = FALSE)
 deaths <- read.csv("https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_deaths_global.csv", stringsAsFactors = FALSE)
 
+# Extract the country names from the data frames
+countries <- unique(confirmed_cases$Country.Region)

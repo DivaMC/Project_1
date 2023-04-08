f399136bf6b63f683e5129ba6617e50cd648fb92
Manuel Vargas <mvargas5897@student.palomar.edu>
2023-04-08 08:12:01 -0700

    Selecting Top 5 Countries

--- Objective_5.R
+++ Objective_5.R
@@ -14,3 +14,18 @@ for (country in countries) {
   confirmed_sums[[country]] <- sum(confirmed_cases[confirmed_cases$Country.Region == country, -(1:4)])
   death_sums[[country]] <- sum(deaths[deaths$Country.Region == country, -(1:4)])
 }
+# Convert the sums into data frames
+confirmed_df <- data.frame(Country = names(confirmed_sums), Confirmed = unlist(confirmed_sums))
+death_df <- data.frame(Country = names(death_sums), Deaths = unlist(death_sums))
+
+# Sort the data frames by the values of confirmed cases and deaths, respectively
+confirmed_df <- confirmed_df[order(-confirmed_df$Confirmed),]
+death_df <- death_df[order(-death_df$Deaths),]
+
+# Select the top 5 countries with the most confirmed cases and deaths
+top_5_confirmed <- head(confirmed_df, 5)
+top_5_deaths <- head(death_df, 5)
+
+
+
+
